920600537d6bf92df2b3f378f9d45be644b2b186
BlackCyber21 <35323386+BlackCyber21@users.noreply.github.com>
2020-12-09 23:17:59 +0000

    Add files via upload

---
+++ web.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import requests
+
+objeto = raw_input("Digite o site: ")
+site = requests.get(objeto)
+status = site.status_code
+
+if (status == 200):
+	print "Pagina Existe"
+else:
+	print "Pagina Inexistente"
